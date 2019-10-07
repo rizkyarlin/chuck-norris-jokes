ecb6cf56b0f5ba559034b01afbab771f8a526b8a
Muhammad Rizky Eka Arlin <rizkhy45@gmail.com>
2019-10-07 22:56:14 +0800

    Test Driven Jokes

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -4,8 +4,20 @@ namespace Rizkyarlin\ChuckNorrisJokes;
 
 class JokeFactory
 {
+    protected $jokes = [
+        'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
+        'Chuck Norris counted to infinity... Twice.',
+        'When the Boogeyman goes to sleep at night he checks his closet for Chuck Norris.'
+    ];
 
-    public function hello(){
-        echo 'chuck norris joke goes here';
+    public function __construct(array $jokes = null)
+    {
+        if ($jokes){
+            $this->jokes = $jokes;
+        }
+    }
+
+    public function getRandomJoke(){
+        return $this->jokes[array_rand($this->jokes)];
     }
 }
--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -4,13 +4,37 @@
 namespace Rizkyarlin\ChuckNorrisJokes\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Rizkyarlin\ChuckNorrisJokes\JokeFactory;
 
 class JokeFactoryTest extends TestCase
 {
     /** @test */
     public function it_returns_a_random_joke()
     {
-        $this->assertTrue(true);
+        $jokes = new JokeFactory([
+            'This is a joke'
+        ]);
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame('This is a joke', $joke);
+
+    }
+
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $chuckNorrisJokes = [
+        'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
+        'Chuck Norris counted to infinity... Twice.',
+        'When the Boogeyman goes to sleep at night he checks his closet for Chuck Norris.'
+        ];
+
+        $jokes = new JokeFactory();
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $chuckNorrisJokes);
 
     }
 }
